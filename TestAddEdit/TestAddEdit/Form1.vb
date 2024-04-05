948c9836d503d859fc03ff24306dc32cbb2e32be
MICHAEL FORTON <MICHAEL FORTON@DESKTOP-QJH73MG>
2024-04-05 15:52:44 +0800

    VIEWING DATA

--- TestAddEdit/TestAddEdit/Form1.vb
+++ TestAddEdit/TestAddEdit/Form1.vb
@@ -3,10 +3,18 @@ Public Class Form1
 
     Public Sub ConnectToDatabase()
         Dim connectionString As String = "Host=10.0.21.71;Port=5432;Username=postgres;Password=root;Database=hms"
+        Dim query As String = "SELECT * FROM shmc.patient_details Limit 100"
+
         Using conn As New NpgsqlConnection(connectionString)
             Try
                 conn.Open()
                 MessageBox.Show("Connected")
+
+                Dim dataAdapter As New NpgsqlDataAdapter(query, conn)
+                Dim dataTable As New DataTable()
+                dataAdapter.Fill(dataTable)
+
+                DataGridView1.DataSource = dataTable
             Catch ex As Exception
                 MessageBox.Show("Not Connected")
             End Try
@@ -18,6 +26,10 @@ Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         ConnectToDatabase()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
     End Sub
 End Class
 
